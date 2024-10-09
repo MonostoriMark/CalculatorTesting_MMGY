f1111664fe1bd7058d6b724d629fd4d4e0e5b12c
Monostori Mark <monostorimark05@gmail.com>
2024-10-09 11:36:05 +0200

    test update

--- test_calculator.py
+++ test_calculator.py
@@ -12,3 +12,15 @@ def test_sub():
     assert sub(-1, 2) == -3
     assert sub(-1, -2) == 1
     assert sub(0, 0) == 0
+    
+def test_mult():
+    assert mult(5, 2) == 10
+    assert mult(-1, 2) == -2
+    assert mult(-1, -2) == 2
+    assert mult(0, 0) == 0
+
+def test_div():
+    assert div(10, 2) == 5
+    assert div(-2, 1) == -2
+    assert div(-6, -2) == 3
+    assert div(0, 1) == 0
